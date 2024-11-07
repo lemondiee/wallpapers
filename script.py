6a39e2373ea9930263cf7abd4ccd2c267aa04bb3
Dos <debasmitbos22@gmail.com>
2024-11-07 07:44:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
-#first make a filder in your Newly created Repository. My folder name is WALLPAPER
+#first make a folder in your Newly created Repository. My folder name is WALLPAPER
 Folder = "WALLPAPER"
 
 #then rename all images to the pattern of 'wallpaper (13)'.
 #On windows you can easily do this using F12. Select all images and press F12. Then rename any one file to the name like 'wallpaper'.
-#all images will be renamed. note the last image name would be the number of files in the folder. In my case, 511.
-TotalIMG = 512 #1 more bcs range is just upto this and not including this
+#all images will be renamed. note the last image name would be the number of files in the folder. In my case, 582.
+TotalIMG = 583 #1 more bcs range is just upto this and not including this
 
 #then upload all images into the folder in github. At a time you can only upload 100 images.
 
